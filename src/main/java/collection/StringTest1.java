7c6f7f5cc43b0883e18329a50e193f09e245c461
wujiandong66 <15652618578@163.com>
2017-03-28 15:58:41 +0800

    1

---
+++ src/main/java/collection/StringTest1.java
@@ -0,0 +1,10 @@
+package collection;
+
+/**
+ * Created by Administrator on 2017/3/28 0028.
+ */
+public class StringTest1 {
+    public static void main(String[] args) {
+        System.out.println("试试");
+    }
+}
